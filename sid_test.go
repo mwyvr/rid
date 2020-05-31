b3aa8d527b97272817640d2b44f13167204e70c0
Mike Watkins <github@mikewatkins.ca>
2020-05-31 12:59:16 -0700

    add examples for godoc/pkg.dev

--- sid_test.go
+++ sid_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"database/sql/driver"
 	enc "encoding"
+	"fmt"
 	"reflect"
 	"sync"
 	"testing"
@@ -406,4 +407,42 @@ func (d *dupes) report(t *testing.T) {
 		t.Errorf("Duplicate Base36 values (keys) found. Total dupes: %d | Total keys: %d\n",
 			total, len(d.count))
 	}
+}
+
+// examples for godoc/pkg.dev
+
+func ExampleNew() {
+	id := sid.New()
+	fmt.Printf(`ID:
+    String()       %s   
+    Milliseconds() %d  
+    Count()        %d 
+    Time()         %v
+    Bytes():       %3v  
+`, id.String(), id.Milliseconds(), id.Count(), id.Time(), id.Bytes())
+}
+
+func ExampleNewWithTime() {
+	id := sid.NewWithTime(time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC))
+	fmt.Printf(`ID:
+    String()       %s
+    Milliseconds() %d
+    Count()        %d 
+    Time()         %v
+    Bytes():       %3v
+`, id.String(), id.Milliseconds(), id.Count(), id.Time().UTC(), id.Bytes())
+}
+
+func ExampleFromString() {
+	id, err := sid.FromString("af1z631jaa0y4")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf(`ID:
+    String()       %s
+    Milliseconds() %d
+    Count()        %d
+    Time()         %v
+    Bytes():       %3v
+`, id.String(), id.Milliseconds(), id.Count(), id.Time().UTC(), id.Bytes())
 }
