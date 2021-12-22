29e4f7efb561587c534fba14cf08b39cf43eed68
Mike Watkins <github@mikewatkins.ca>
2021-12-22 15:42:56 -0800

    revert to 8 byte IDs

--- sid.go
+++ sid.go
@@ -1,65 +1,29 @@
 /*
-Package sid provides a no-configuration required unique ID generator for
-applications with modest needs.
+Package sid provides a unique-enough ID generator for applications with modest
+(read: non-distributed), needs.
 
-sid's 8-byte value can be stored directly as a 64 bit integer;both the byte
-value and string representation are k-sortable.
+    id := sid.New()
+    fmt.Printf("%s", id) // af1zwtepacw38
 
-sid produces URL and human-friendly (readability and double-click), compact IDs
-(64 bit integer) with base32 encoded string (13 character) representations that
-look like:
+A sid ID is 8-byte value; it could optionally be stored as a 64 bit integer.
 
-    af1zwtepacw38
+    // af1zwtepacw38
+    sid.FromString("af1zwtepacw38") == id   // true
+    fmt.Println(id[:])                      // [1 125 227 253 59 110 47 62]
 
-Each ID's 8-byte binary representation:
+Each ID's 8-byte binary representation: id{1, 111, 89, 64, 140, 0, 165, 159} is
+comprised of a:
 
-    001 125 209 022 154 224 151 086
+- 6-byte timestamp value representing milliseconds since the Unix epoch
+- 2-byte concurrency-safe counter (test included); maxCounter = uint16(65535)
 
-is comprised of a:
+IDs are chronologically sortable with a minor tradeoff in millisecond-level
+sortability made for improved randomness in the trailing counter value.
 
-    6-byte timestamp value representing milliseconds since the Unix epoch
-    2-byte concurrency-safe counter (test included); maxCounter = uint16(65535)
-
-The counter is initialized at a random value at initialization.
-
-*Modest Needs*
-
-sid is intended for single process, single machine apps - perhaps using Go
-friendly datastores like BoltDB, Badger or abstractions on top of either like
-Genji.
-
-The 2-byte concurrency-safe counter is a uint16, meaning 65,535 unique IDs can
-be produced per millisecond or 1 ID every 16 nanoseconds. On the author's
-hardware it takes more than 50ns to produce an ID, another 50ns to encode it,
-longer yet to shove data into a datastore, so there's little chance of collision
-- concurrency tests show this.
-
-The base32 encoding utilizes a customized alphabet based upon that popularized
-by Crockford who replaced the more easily misread (by humans) i, o, l, and u
-with the more easily read w, x, y, z. In sid, digits have been moved to the tail
-of the character set to avoid having a leading zero for a great many years.
-
-ID implements a number of common interfaces including package sql's
-driver.Valuer, sql.Scanner, TextMarshaler, TextUnmarshaler, json.Marshaler,
-json.Unmarshaler, and  Stringer.
-
-Original source of inspiration:
-https://blog.kowalczyk.info/article/JyRZ/generating-good-unique-ids-in-go.html
+The String() representation us base32 encoded using a modified Crockford inspired alphabet.
 
 Acknowledgement: Much of this package is based on the globally-unique capable
-rs/xid package which itself levers ideas from mongodb. See
-https://github.com/rs/xid. I'd use xid if I had a fleet of apps on machines
-spread around the world working in unison on a common datastore.
-
-Comparisons: github.com/solutionroute/sid/v3:    af87cfy46ajbxf40
-    github.com/rs/xid:                  9bsv0s091sd002o20hk0
-    github.com/segmentio/ksuid:         ZJkWubTm3ZsHZNs7FGt6oFvVVnD
-    github.com/kjk/betterguid:          -HIVJnL-rmRZno06mvcV
-    github.com/oklog/ulid:              014KG56DC01GG4TEB01ZEX7WFJ
-    github.com/chilts/sid:              1257894000000000000-4601851300195147788
-    github.com/lithammer/shortuuid:     DWaocVZPEBQB5BRMv6FUsZ
-    github.com/google/uuid:             fa931eb3-cdc7-46a1-ae94-eb1b523203be
-
+rs/xid package which itself levers ideas from mongodb. See https://github.com/rs/xid.
 */
 package sid
 
@@ -74,61 +38,55 @@ import (
 	"unsafe"
 )
 
-// ID represents a locally unique identifier
+// ID represents a locally unique and random-enough yet sortable identifier
 type ID [rawLen]byte
 
 const (
 	rawLen     = 8             // bytes
-	encodedLen = 13            // of base32 representation
-	maxCounter = uint32(65535) // IDs per millisecond
+	encodedLen = 13            // base32 representation
+	maxCounter = uint32(65535) // max IDs per millisecond, not achievable and safe
 
-	//  ID string representations are Base32-encoded using a
-	// character set proposed by *Crockford, but with digits following to avoid
-	// producing IDs with leading zeros for many years.
-	//
-	//  *Crockford character set (i, o, l, u were removed and w, x, y, z added).
-	//
-	// encoding/Base32 standard for comparison:
-	//        "0123456789abcdefghijklmnopqrstuv"
+	/*
+		ID string representations are base32-encoded using a character set
+		inspired by Crockford's (i, o, l, u removed and w, x, y, z added). sid's
+		character set has digits moved to the end  to avoid producing IDs with
+		leading zeros for many years.
+
+		encoding/Base32 for comparison:
+		          "0123456789abcdefghijklmnopqrstuv"
+	*/
 	charset = "abcdefghjkmnpqrstvwxyz0123456789"
 )
 
 var (
-	// counter is atomically updated. Initialized at a random value between 0
-	// and maxCounter, and is designed to rollover back to 0 (1, actually), too.
-
-	counter uint32 // uint32 to take advantage of atomic pkg
-	last    uint64 // last millisecond an id was generated within
-
-	// ErrInvalidID returned on attempt to decode an invalid ID character
-	// representation (length or character set).
-	ErrInvalidID = errors.New("sid: invalid id")
-
 	// ID{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 	nilID ID
 
-	// dec is the decoding map for base32 encoding
+	counter uint32 // this is uint32 to take advantage of atomic.CompareAndSwap...
+	last    uint64 // the last millisecond since Unix epoch when an id was generated
+
+	ErrInvalidID = errors.New("sid: invalid id")
+
+	// dec is the decoding map for base32 encoding; currently only used for error checking
 	dec      [256]byte
 	encoding = base32.NewEncoding(charset).WithPadding(-1)
 )
 
 func init() {
-	// We don't need crypto/rand
-	rand.Seed(time.Now().UnixNano())
-	counter = randInt()
-
-	// create the base32 decoding table (used for error checking) from the charset
+	// initialize the base32 decoding table
 	for i := 0; i < len(dec); i++ {
 		dec[i] = 0xFF
 	}
 	for i := 0; i < len(charset); i++ {
 		dec[charset[i]] = byte(i)
 	}
+
+	// We don't need crypto/rand for a random-ish solution
+	rand.Seed(time.Now().UnixNano())
+	counter = randInt()
 }
 
-// New returns a new ID using the current time. ID's stored as []byte or as an
-// encoded string have the same time resolution: milliseconds from the Unix
-// epoch.
+// New returns a new ID using the current time; IDs represent millisecond time resolution.
 func New() ID {
 	return NewWithTime(time.Now())
 }
@@ -138,13 +96,13 @@ func NewWithTime(tm time.Time) ID {
 	var id ID
 	// timestamp truncated to milliseconds
 	var ms = uint64(tm.Unix())*1000 + uint64(tm.Nanosecond()/int(time.Millisecond))
-	// if the current millisecond is not the same as the `last` value,
-	// assign a new random value to counter
+
+	// Package atomic's operations are concurrency safe.
 	if ms != atomic.LoadUint64(&last) {
-		atomic.StoreUint64(&last, ms)
-		atomic.StoreUint32(&counter, randInt())
+		atomic.StoreUint64(&last, ms)           // we're in a new ms, save it
+		atomic.StoreUint32(&counter, randInt()) // randomly initialize the counter
 	}
-	// id is 10 bytes:
+	// Assemble the 8 byte ID
 	// 6 bytes of time, to millisecond
 	id[0] = byte(ms >> 40)
 	id[1] = byte(ms >> 32)
@@ -153,11 +111,11 @@ func NewWithTime(tm time.Time) ID {
 	id[4] = byte(ms >> 8)
 	id[5] = byte(ms)
 
-	// These operations are concurrency safe.
-	// if counter hits max uint16 value, roll over
+	// If counter hits max uint16 value, roll over
 	atomic.CompareAndSwapUint32(&counter, maxCounter, 0)
 	// increment by 1
 	ct := atomic.AddUint32(&counter, 1)
+	// 2 bytes for the counter
 	id[6] = byte(ct >> 8)
 	id[7] = byte(ct)
 
