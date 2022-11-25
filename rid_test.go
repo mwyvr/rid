c125bad9395da9fe20bb2463e23dfb50ee741ea8
Mike Watkins <github@mikewatkins.ca>
2022-11-25 07:27:08 -0800

    basic test coverage

--- rid_test.go
+++ rid_test.go
@@ -4,8 +4,8 @@ package rid
 
 import (
 	"bytes"
-	"database/sql/driver"
-	enc "encoding"
+	// "database/sql/driver"
+	// enc "encoding"
 	"fmt"
 	"reflect"
 	"sync"
@@ -20,331 +20,254 @@ var (
 	numIter       = 500  // id creation/routine
 )
 
-type idTest struct {
-	name         string
-	valid        bool
-	id           ID
-	rawBytes     []byte
-	seconds      int64
-	random       uint32
-	b32          string
+type idParts struct {
+    id ID 
+    timestamp int64
+    machine []byte
+    pid     uint16
+    random  uint32
 }
 
-// TODO add date values in for direct comparison
-var testIDS = []idTest{
-	{
-		"nilID",
-		false,
-		nilID,
-		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+var IDs = []idParts {
+    // sorted should be IDs 1, 2, 0
+    {
+        // [ce0dmp0s249v4q507980] seconds:1669388888 random:1554004572 machine:[0x19, 0x11] pid:5042 time:2022-11-25 07:08:08 -0800 PST 
+        ID{0x63, 0x80, 0xda, 0x58, 0x19, 0x11, 0x13, 0xb2, 0x5c, 0xa0, 0x3a, 0x50},
+		1669388888,
+		[]byte{0x19, 0x11},
+		5042,
+		1554004572,
+	},
+    {
+		ID{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 		0,
+		[]byte{0x00, 0x00},
+		0x0000,
 		0,
-		"0000000000000000",
 	},
-	{
-		// epoch time plus a counter of one to avoid being
-		// equal to nilID, which is far as counter should never
-		// be 0
-		"min value 1970-01-01 00:00:00 +0000 UTC",
-		true,
-		ID{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
-		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01},
-		0,
-		1,
-		"0000000000002",
-	},
-	{
-		"max value in the year 10889 see you then",
-		true,
-		ID{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
-		[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
-		281474976710655,
-		65535,
-		"zzzzzzzzzzzzy",
-	},
-	{
-		"fail on FromString / FromBytes / decode - value mismatch",
-		false,
-		ID{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
-		[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xaa},
-		281474976710655,
-		65535,
-		"1234567890abc",
-	},
-	{
-		"fail on FromString, FromBytes len mismatch",
-		false,
-		ID{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
-		[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xaa},
-		281474976710655,
-		65535,
-		"zzzz",
-	},
-	{
-		"must fail MarshalText (decode test - invalid base32 chars)",
-		false,
-		ID{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF},
-		0,
-		1,
-		"zzzuzzzzzzzzt",
+    {
+        // [ce0djy0s248ra7qrh140] seconds:1669388664 random:519604254 machine:[0x19, 0x11] pid:4485 time:2022-11-25 07:04:24 -0800 PST 
+        ID{0x63, 0x80, 0xd9, 0x78, 0x19, 0x11, 0x11, 0x85, 0x1e, 0xf8, 0x88, 0x48},
+		1669388664,
+		[]byte{0x19, 0x11},
+		4485,
+		519604254,
 	},
 }
 
+func TestIDPartsExtraction(t *testing.T) {
+	for i, v := range IDs {
+		t.Run(fmt.Sprintf("Test%d", i), func(t *testing.T) {
+			if got, want := v.id.Time(), time.Unix(v.timestamp, 0); got != want {
+				t.Errorf("Time() = %v, want %v", got, want)
+			}
+			if got, want := v.id.Machine(), v.machine; !bytes.Equal(got, want) {
+				t.Errorf("Machine() = %v, want %v", got, want)
+			}
+			if got, want := v.id.Pid(), v.pid; got != want {
+				t.Errorf("Pid() = %v, want %v", got, want)
+			}
+			if got, want := v.id.Random(), v.random; got != want {
+				t.Errorf("Random() = %v, want %v", got, want)
+			}
+		})
+	}
+}
 
 func TestNew(t *testing.T) {
-	id := New()
-	if id == nilID {
-		t.Errorf("New() produced a nilID")
+	// Generate 10 ids
+	ids := make([]ID, 100)
+	for i := 0; i < 100; i++ {
+		ids[i] = New()
 	}
-}
+	for i := 1; i < 100; i++ {
+		prevID := ids[i-1]
+		id := ids[i]
+		// Test for uniqueness among all other 9 generated ids
+		for j, tid := range ids {
+			if j != i {
+                // can't use ID.Compare for this test, need to compare entire ID[:]
+                if bytes.Compare(id[:], tid[:]) == 0 {
 
-func TestNewWithTime(t *testing.T) {
-	// must match
-	id := NewWithTime(time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC))
-	if id.String() != "05qnwsq800002" {
-		t.Errorf("ID.NewWithTime().String() not matching got %v, want %v",
-			id.String(), "05qnwsq800002")
-	}
-}
-
-func TestID_IsNil(t *testing.T) {
-	id := New()
-	if id.IsNil() {
-		t.Errorf("ID.IsNil() returned %v, want %v", id.IsNil(), false)
-	}
-	id = ID{}
-	if !id.IsNil() {
-		t.Errorf("ID.IsNil() returned %v, want %v", id.IsNil(), false)
-	}
-}
-
-func TestID_Seconds(t *testing.T) {
-	id := NewWithTime(time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC))
-	if m := id.Seconds(); m != int64(1577836800000) {
-		t.Errorf("ID.Seconds() got %v want %v", m, 1577836800000)
-	}
-}
-
-func TestID_Bytes(t *testing.T) {
-	id, err := FromString("05yykgvzqfzzy")
-	if err != nil {
-		t.Error(err)
-	}
-	want := []byte{1, 125, 233, 195, 127, 187, 255, 255}
-	if b := id.Bytes(); bytes.Equal(b, want) != true {
-		t.Errorf("ID.Bytes() got %v want %v", b, want)
-	}
-}
-
-func TestID_Components(t *testing.T) {
-	// for completeness
-	for _, tt := range testIDS {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.id.String(); (got != tt.b32) && (tt.valid != false) {
-				t.Errorf("ID.String() = %v, want %v", got, tt.b32)
+				// if id.Compare(tid) == 0 {
+					t.Errorf("generated ID is not unique (%d/%d)\n%v", i, j, ids)
+				}
 			}
-		})
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.id.Seconds(); (got != tt.seconds) && (tt.valid != false) {
-				t.Errorf("ID.Seconds() = %v %v, want %v", got, tt.id[:], tt.seconds)
-			}
-		})
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.id.Random(); (got != tt.random) && (tt.valid != false) {
-				t.Errorf("ID.Random() = %v, want %v", got, tt.random)
-			}
-		})
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.id.Bytes(); !reflect.DeepEqual(got, tt.id[:]) {
-				t.Errorf("ID.Bytes() = %#v, want %#v", got, tt.id)
-			}
-		})
+		}
+		// Check that timestamp was incremented and is within 30 seconds of the previous one
+		secs := id.Time().Sub(prevID.Time()).Seconds()
+		if secs < 0 || secs > 30 {
+			t.Error("wrong timestamp in generated ID")
+		}
+		// Check that machine ids are the same
+		if !bytes.Equal(id.Machine(), prevID.Machine()) {
+			t.Error("machine ID not equal")
+		}
+		// Check that pids are the same
+		if id.Pid() != prevID.Pid() {
+			t.Error("pid not equal")
+		}
 	}
 }
 
-func TestID_Time(t *testing.T) {
-	id, err := FromString("0000000000000")
-	date := id.Time().UTC()
-	if err != nil {
-		t.Error(err)
+func TestIDString(t *testing.T) {
+	id := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
+	if got, want := id.String(), "9p4e2pv0yj3e8a215q4g"; got != want {
+		t.Errorf("String() = %v, want %v", got, want)
 	}
-	y, m, d := date.Date()
-	if (y != 1970) || (m != 1) || (d != 1) {
-		t.Errorf("ID.Time() returned %d, %d, %d; want 1970,1,1", y, m, d)
-	}
-	// now
-	id = NewWithTime(time.Now())
-	if int64(id.Time().UnixNano()/1e6) != id.Seconds() {
-		t.Errorf("ID.Time() UnixNano()/1e6 != id.Seconds")
+}
+
+func TestIDEncode(t *testing.T) {
+	id := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
+	text := make([]byte, encodedLen)
+	if got, want := string(id.Encode(text)), "9p4e2pv0yj3e8a215q4g"; got != want {
+		t.Errorf("Encode() = %v, want %v", got, want)
 	}
 }
 
 func TestFromString(t *testing.T) {
-	for _, tt := range testIDS {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := FromString(tt.b32)
-			if tt.valid && (err != nil) {
-				t.Errorf("FromString() error = %v, is valid %v", err, tt.valid)
-				return
-			}
-			_ = err
-			if tt.valid && !reflect.DeepEqual(got, tt.id) {
-				t.Errorf("FromString() = %v, want %v", got, tt.id)
-			}
-		})
-	}
-	// callers should lowercase.
-	got, err := FromString("aaaaaaaaaaaaA")
-	if err == nil {
-		t.Errorf("Should be an error")
-	} else if err != ErrInvalidID {
-		t.Errorf("FromString() = %v, want err %v got %v", got, err, ErrInvalidID)
-	}
-	// decoding the nilID value is legit
-	got, err = FromString("aaaaaaaaaaaaa")
+	got, err := FromString("9p4e2pv0yj3e8a215q4g")
 	if err != nil {
-		t.Errorf("FromString(\"aaaaaaaaaaaaa\") nilID value failed, got %v, %v", got, err)
+		t.Fatal(err)
+	}
+	want := ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
+	if got != want {
+		t.Errorf("FromString() = %v, want %v", got, want)
 	}
 }
 
-func TestFromBytes(t *testing.T) {
-	for _, tt := range testIDS {
+func TestFromStringInvalid(t *testing.T) {
+	_, err := FromString("invalid")
+	if err != ErrInvalidID {
+		t.Errorf("FromString(invalid) err=%v, want %v", err, ErrInvalidID)
+	}
+	id, err := FromString("ce0cnw0s25j1ksgsilou") // i, l, o, u never in our IDs
+	if err != ErrInvalidID {
+		t.Errorf("FromString(ce0cnw0s25j1ksgsilou - invalid chars) err=%v, want %v", err, ErrInvalidID)
+	}
+	if id != nilID {
+		t.Errorf("FromString() =%v, there want %v", id, nilID)
+	}
+}
+
+func TestID_IsNil(t *testing.T) {
+	tests := []struct {
+		name string
+		id   ID
+		want bool
+	}{
+		{
+			name: "ID not nil",
+			id:   New(),
+			want: false,
+		},
+		{
+			name: "Nil ID",
+			id:   ID{},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := FromBytes(tt.rawBytes)
-			if tt.valid && (err != nil) {
-				t.Errorf("FromBytes() error = %v, wantErr %v", err, tt.valid)
-				return
-			}
-			if tt.valid && !reflect.DeepEqual(got, tt.id) {
-				t.Errorf("FromBytes() = %v, want %v", got, tt.id)
+			if got, want := tt.id.IsNil(), tt.want; got != want {
+				t.Errorf("IsNil() = %v, want %v", got, want)
 			}
 		})
 	}
-	// nilID byte value is unusual but legit
-	got, err := FromBytes([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
+
+func TestNilID(t *testing.T) {
+	got := ID{}
+	if want := NilID(); !reflect.DeepEqual(got, want) {
+		t.Error("NilID() not equal ID{}")
+	}
+}
+
+func TestNilID_IsNil(t *testing.T) {
+	if !NilID().IsNil() {
+		t.Error("NilID().IsNil() is not true")
+	}
+}
+
+func TestFromBytes_Invariant(t *testing.T) {
+	want := New()
+	got, err := FromBytes(want.Bytes())
 	if err != nil {
-		t.Errorf("FromBytes([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}) nilID value failed, got %v, %v", got, err)
+		t.Fatal(err)
 	}
-	// invalid len
-	got, err = FromBytes([]byte{0x12, 0x34})
-	if err == nil {
-		t.Errorf("FromBytes([]byte{0x12, 0x34}) got %v, err==nil, %v", got, err)
+	if got.Compare(want) != 0 {
+		t.Error("FromBytes(id.Bytes()) != id")
 	}
 }
 
-func Test_encode(t *testing.T) {
-	for _, tt := range testIDS {
-		t.Run(tt.name, func(t *testing.T) {
-			dest := make([]byte, encodedLen)
-			encode(dest, tt.id[:])
-			if len(dest) != encodedLen {
-				t.Errorf("encode: wrong length got %d want %d", len(dest), encodedLen)
-				return
-			}
-			if tt.valid && string(dest) != tt.b32 {
-				t.Errorf("encode: wrong output got %s want %s", dest, tt.b32)
-			}
-		})
+func TestFromBytes_InvalidBytes(t *testing.T) {
+	cases := []struct {
+		length     int
+		shouldFail bool
+	}{
+		{11, true},
+		{12, false},
+		{13, true},
+	}
+	for _, c := range cases {
+		b := make([]byte, c.length)
+		_, err := FromBytes(b)
+		if got, want := err != nil, c.shouldFail; got != want {
+			t.Errorf("FromBytes() error got %v, want %v", got, want)
+		}
 	}
 }
 
-func Test_decode(t *testing.T) {
-	id := &ID{}
-	// there really are no checks in decode; they happen in UnmarshalText,
-	// the only caller of decode(). For code coverage:
-	decode(id, []byte("05yykgvzqc002"[:]))
-	if id.Random() != 1 {
-		t.Errorf("decode produced an anomoly: %#v", id)
+var IDList = []ID{IDs[0].id, IDs[1].id, IDs[2].id}
+
+func TestSorter_Len(t *testing.T) {
+	if got, want := sorter([]ID{}).Len(), 0; got != want {
+		t.Errorf("Len() %v, want %v", got, want)
+	}
+	if got, want := sorter(IDList).Len(), 3; got != want {
+		t.Errorf("Len() %v, want %v", got, want)
 	}
 }
 
-func TestID_UnmarshalText(t *testing.T) {
-	for _, tt := range testIDS {
-		t.Run(tt.name, func(t *testing.T) {
-			// Ensure that ID fulfills the Interface
-			var _ enc.TextUnmarshaler = &ID{}
-			text := []byte(tt.b32[:])
-			if err := tt.id.UnmarshalText(text); err != nil {
-				if tt.valid { // shouldn't be
-					t.Errorf("ID.UnmarshalText() error = %v, want %v", err, tt.id[:])
-				}
-				if !tt.valid && err != ErrInvalidID {
-					t.Errorf("ID.UnmarshalText() error = %v, want %v", err, ErrInvalidID)
-				}
-			}
-		})
+func TestSorter_Less(t *testing.T) {
+	sorter := sorter(IDList)
+	if !sorter.Less(1, 0) {
+		t.Errorf("Less(1, 0) not true")
+	}
+	if sorter.Less(2, 1) {
+		t.Errorf("Less(2, 1) true")
+	}
+	if sorter.Less(0, 0) {
+		t.Errorf("Less(0, 0) true")
 	}
 }
 
-func TestID_MarshalText(t *testing.T) {
-	// ensure ID implements the interface
-	var _ enc.TextMarshaler = &ID{}
-	for _, tt := range testIDS {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.id.MarshalText()
-			if tt.valid && (err != nil) {
-				t.Errorf("ID.MarshalText() error = %v, wantErr %v", err, tt.valid)
-				return
-			}
-			if tt.valid && string(got) != tt.b32 {
-				t.Errorf("ID.MarshalText() = %v, want %v", got, tt.b32)
-			}
-		})
+func TestSorter_Swap(t *testing.T) {
+	ids := make([]ID, 0)
+	ids = append(ids, IDList...)
+	sorter := sorter(ids)
+	sorter.Swap(0, 1)
+	if got, want := ids[0], IDList[1]; !reflect.DeepEqual(got, want) {
+		t.Error("ids[0] != IDList[1]")
+	}
+	if got, want := ids[1], IDList[0]; !reflect.DeepEqual(got, want) {
+		t.Error("ids[1] != IDList[0]")
+	}
+	sorter.Swap(2, 2)
+	if got, want := ids[2], IDList[2]; !reflect.DeepEqual(got, want) {
+		t.Error("ids[2], IDList[2]")
 	}
 }
 
-func TestID_Value(t *testing.T) {
-	// ensure ID implements the interface
-	var _ driver.Valuer = &ID{}
-	for _, tt := range testIDS {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.id.Value()
-			if tt.valid && (err != nil) {
-				t.Errorf("ID.Value() error = %v, is valid %v", err, tt.valid)
-				return
-			}
-			if tt.valid && !reflect.DeepEqual(got, tt.b32) {
-				t.Errorf("ID.Value() = %v, want %v test: %v", got, tt.b32, tt)
-			}
-		})
-	}
-	// nilID
-	val, err := nilID.Value()
-	if (val != nil) && (err != nil) {
-		t.Errorf("ID.Value(nilID) want nil, nil, got %v, %v", val, err)
+func TestSort(t *testing.T) {
+	ids := make([]ID, 0)
+	ids = append(ids, IDList...)
+	Sort(ids)
+	if got, want := ids, []ID{IDList[1], IDList[2], IDList[0]}; !reflect.DeepEqual(got, want) {
+		t.Fail()
 	}
 }
-
-func TestID_Scan(t *testing.T) {
-	for _, tt := range testIDS {
-		t.Run(tt.name, func(t *testing.T) {
-			// Scan string
-			if err := tt.id.Scan(tt.b32); tt.valid && (err != nil) {
-				t.Errorf("ID.Scan() error = %v, valid? %v", err, tt.valid)
-			}
-			// Scan []byte
-			bs := []byte(tt.b32)
-			if err := tt.id.Scan(bs); tt.valid && (err != nil) {
-				t.Errorf("ID.Scan() error = %v, valid? %v", err, tt.valid)
-			}
-		})
-	}
-	// nil
-	id := New()
-	if err := id.Scan(nil); err != nil {
-		t.Errorf("ID.Scan() error = %v, should return nilID", err)
-	}
-	if bytes.Equal(id[:], nilID[:]) != true {
-		t.Errorf("ID.Scan() got %v, should return nilID %v", id, nilID)
-	}
-	// unsupported
-	id = ID{}
-	if err := id.Scan(false); err == nil {
-		t.Errorf("ID.Scan() error = %v, should not convert bool", err)
-	}
-}
-
 
 // Benchmarks
 func BenchmarkIDNew(b *testing.B) {
@@ -395,33 +318,33 @@ func ExampleFromString() {
 	// [05yx13hj9kq4g] ms:1639881519692 count:61000 time:2021-12-18 18:38:39.692 -0800 PST id:{1, 125, 208, 142, 50, 76, 238, 72}
 }
 
-func TestID_MarshalJSON(t *testing.T) {
-	if got, err := nilID.MarshalJSON(); string(got) != "null" {
-		t.Errorf("ID.MarshalJSON() of nilID error = %v, got %v", err, got)
-	}
-	if got, err := (ID{1, 125, 208, 142, 50, 76, 238, 72}).MarshalJSON(); string(got) != "\"05yx13hj9kq4g\"" {
-		if err != nil {
-			t.Errorf("ID.MarshalJSON() err %v marshaling %v", err, "\"05yx13hj9kq4g\"")
-		}
-		t.Errorf("ID.MarshalJSON() got %v want %v", string(got), "\"05yx13hj9kq4g\"")
-	}
-}
-
-func TestID_UnmarshalJSON(t *testing.T) {
-	var id ID
-	err := id.UnmarshalJSON([]byte("null"))
-	if err != nil {
-		t.Errorf("ID.UnmarshalJSON() of null, error = %v", err)
-	}
-	if id != nilID {
-		t.Errorf("ID.UnmarshalJSON() error = %v", err)
-	}
-	// 2020...
-	text := []byte("\"05yykgvzqc002\"")
-	if err = id.UnmarshalJSON(text); err != nil {
-		t.Errorf("ID.UnmarshalJSON() error = %v", err)
-
-	} else if id != (ID{1, 125, 233, 195, 127, 187, 0, 1}) {
-		t.Errorf("ID.UnmarshalJSON() of %v, got %v", text, id.String())
-	}
-}
+// func TestID_MarshalJSON(t *testing.T) {
+// 	if got, err := nilID.MarshalJSON(); string(got) != "null" {
+// 		t.Errorf("ID.MarshalJSON() of nilID error = %v, got %v", err, got)
+// 	}
+// 	if got, err := (ID{1, 125, 208, 142, 50, 76, 238, 72}).MarshalJSON(); string(got) != "\"05yx13hj9kq4g\"" {
+// 		if err != nil {
+// 			t.Errorf("ID.MarshalJSON() err %v marshaling %v", err, "\"05yx13hj9kq4g\"")
+// 		}
+// 		t.Errorf("ID.MarshalJSON() got %v want %v", string(got), "\"05yx13hj9kq4g\"")
+// 	}
+// }
+//
+// func TestID_UnmarshalJSON(t *testing.T) {
+// 	var id ID
+// 	err := id.UnmarshalJSON([]byte("null"))
+// 	if err != nil {
+// 		t.Errorf("ID.UnmarshalJSON() of null, error = %v", err)
+// 	}
+// 	if id != nilID {
+// 		t.Errorf("ID.UnmarshalJSON() error = %v", err)
+// 	}
+// 	// 2020...
+// 	text := []byte("\"05yykgvzqc002\"")
+// 	if err = id.UnmarshalJSON(text); err != nil {
+// 		t.Errorf("ID.UnmarshalJSON() error = %v", err)
+//
+// 	} else if id != (ID{1, 125, 233, 195, 127, 187, 0, 1}) {
+// 		t.Errorf("ID.UnmarshalJSON() of %v, got %v", text, id.String())
+// 	}
+// }
