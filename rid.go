d3abfcf0d8d5341d2363f783d8f7f0571fc85b7e
Mike Watkins <github@mikewatkins.ca>
2022-11-25 08:07:46 -0800

    gofmt

--- rid.go
+++ rid.go
@@ -1,40 +1,46 @@
 /*
-Package rid provides a unique-enough ID generator for
-applications with modest (meaning non-distributed) needs. 
-
-rid can generate more than 4 billion possible random IDs per second, per
-process, per machine. Duplication in practical use is unlikely. Ymmv.
-
-    id := rid.New()
-    fmt.Printf("%s", id) 
-// TODO replace all the examples once fully tested
-
-Each ID's 12-byte binary representation is comprised of a:
-
-- 4-byte timestamp value representing seconds since the Unix epoch
-- 2-byte machine ID
-- 2-byte process ID
-- 4-byte random value
+Package rid provides a hybrid k-sortable random ID generator. The 12 byte
+binary ID string representation is a 20-character long, URL-friendly/Base32
+encoded, mostly k-sortable (to the second resolution) identifier.
 
 IDs are chronologically sortable to the second, with a tradeoff in fine-grained
 sortability due to the trailing random value component.
 
-The String() representation is Base32 encoded using a modified Crockford
+Each ID's 12-byte binary representation is comprised of a:
+
+  - 4-byte timestamp value representing seconds since the Unix epoch
+  - 2-byte machine ID
+  - 2-byte process ID
+  - 4-byte random value with 4,294,967,295 possibilities; collision detection
+    guarantees the random value is unique for a given timestamp+machine ID+process ID.
+
+The String() representation of ID is Base32 encoded using a modified Crockford
 inspired alphabet.
 
-Acknowledgement: Much of this package is based on the globally-unique capable
-rs/xid package which itself levers ideas from mongodb. See https://github.com/rs/xid.
+Example:
+
+	id := rid.New()
+	fmt.Printf("%s", id) //  cdym59rs24a5g86efepg
+
+Acknowledgement: This package borrows heavily from rs/xid
+(https://github.com/rs/xid), a capable unique ID package which itself levers
+ideas from MongoDB (https://docs.mongodb.com/manual/reference/method/ObjectId/).
+
+Where rid differs from xid is in the use of (admittedly slower) random number
+generation as opposed to a trailing counter for the last 4 bytes of the ID.
 */
 package rid
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/rand"
 	"database/sql/driver"
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"crypto/md5"
-	"crypto/rand"
-    "os"
+	"os"
+	"sort"
 	"time"
 	"unsafe"
 )
@@ -43,13 +49,13 @@ import (
 type ID [rawLen]byte
 
 const (
-	rawLen     = 12                  // binary representation
-	encodedLen = 20                 // base32 representation
-    // ID string representations are base32-encoded using a character set
-    // inspired by Crockford: i, l, o, u removed and w, x, y, z added.
-    // 
-    // encoding/Base32 charset for comparison:
-    //        "0123456789abcdefghijklmnopqrstuv"
+	rawLen     = 12 // binary representation
+	encodedLen = 20 // base32 representation
+	// ID string representations are base32-encoded using a character set
+	// inspired by Crockford: i, l, o, u removed and w, x, y, z added.
+	//
+	// encoding/Base32 charset for comparison:
+	//         "0123456789abcdefghijklmnopqrstuv"
 	encoding = "0123456789abcdefghjkmnpqrstvwxyz"
 )
 
@@ -57,8 +63,8 @@ var (
 	// ID{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 	nilID ID
 
-    // BySecond method generates unique random numbers per second clock tick
-    rgenerator = &rng {lastUpdated: 0, exists: make(map[uint32]bool)}
+	// BySecond method generates unique random numbers per second clock tick
+	rgenerator = &rng{lastUpdated: 0, exists: make(map[uint32]bool)}
 
 	// machineId stores a md5 hash of the machine identifier or hostname
 	machineID = readMachineID()
@@ -67,10 +73,9 @@ var (
 	pid = os.Getpid()
 
 	// dec is the decoding map for base32 encoding
-	dec      [256]byte
+	dec [256]byte
 
 	ErrInvalidID = errors.New("rid: invalid id")
-	ErrInvalidLength = errors.New("rid: invalid encoded length")
 )
 
 func init() {
@@ -101,7 +106,7 @@ func NewWithTime(tm time.Time) ID {
 	id[6] = byte(pid >> 8)
 	id[7] = byte(pid)
 	// 4 bytes for the random value, big endian
-    rv := rgenerator.BySecond(tm.Unix())
+	rv := rgenerator.BySecond(tm.Unix())
 	id[8] = byte(rv >> 24)
 	id[9] = byte(rv >> 16)
 	id[10] = byte(rv >> 8)
@@ -115,12 +120,28 @@ func (id ID) IsNil() bool {
 	return id == nilID
 }
 
+// Alias of IsNil
+func (id ID) IsZero() bool {
+	return id.IsNil()
+}
+
+// NilID returns a zero value for `rid.ID`.
+func NilID() ID {
+	return nilID
+}
+
 // String returns the custom base32 encoded representation of ID.
 func (id ID) String() string {
 	text := make([]byte, encodedLen)
 	encode(text, id[:])
 	// avoids an allocation
 	return *(*string)(unsafe.Pointer(&text))
+}
+
+// Encode encodes the id using base32 encoding, writing 20 bytes to dst and return it.
+func (id ID) Encode(dst []byte) []byte {
+	encode(dst, id[:])
+	return dst
 }
 
 // encode by unrolling the stdlib base32 algorithm + removing all safe checks
@@ -182,7 +203,7 @@ func (id ID) Pid() uint16 {
 
 // Random returns the random component of the ID.
 func (id ID) Random() uint32 {
-    b := id[8:12]
+	b := id[8:12]
 	return uint32(uint32(b[0])<<24 | uint32(b[1])<<16 | uint32(b[2])<<8 | uint32(b[0]))
 }
 
@@ -208,15 +229,15 @@ func FromBytes(b []byte) (ID, error) {
 // All decoding is called from here.
 func (id *ID) UnmarshalText(text []byte) error {
 	if len(text) != encodedLen {
-		return ErrInvalidLength
+		return ErrInvalidID
 	}
 	for _, c := range text {
-        // invalid characters (not in encoding)
+		// invalid characters (not in encoding)
 		if dec[c] == 0xFF {
 			return ErrInvalidID
 		}
 	}
-    if !decode(id, text) {
+	if !decode(id, text) {
 		*id = nilID
 		return ErrInvalidID
 	}
@@ -305,7 +326,6 @@ func (id *ID) UnmarshalJSON(text []byte) error {
 	return id.UnmarshalText(text[1 : len(text)-1])
 }
 
-
 // readMachineId generates machine id and puts it into the machineId global
 // variable. If this function fails to get the hostname, and the fallback
 // fails, it will cause a runtime error.
@@ -328,3 +348,35 @@ func readMachineID() []byte {
 	return id
 }
 
+// Compare returns an integer comparing two IDs, comparing only the first 8 bytes:
+// - 4-byte timestamp
+// - 2-byte machine ID
+// - 2-byte process ID
+// ... while ignoring the trailing:
+// - 4-byte random value
+// Otherwise, it behaves just like `bytes.Compare`.
+// The result will be 0 if two IDs are identical, -1 if current id is less than
+// the other one, and 1 if current id is greater than the other.
+func (id ID) Compare(other ID) int {
+	return bytes.Compare(id[:9], other[:9])
+}
+
+type sorter []ID
+
+func (s sorter) Len() int {
+	return len(s)
+}
+
+func (s sorter) Less(i, j int) bool {
+	return s[i].Compare(s[j]) < 0
+}
+
+func (s sorter) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// Sort sorts an array of IDs inplace.
+// It works by wrapping `[]ID` and use `sort.Sort`.
+func Sort(ids []ID) {
+	sort.Sort(sorter(ids))
+}
