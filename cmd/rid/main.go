cb0a78df882ae91fe5dcfdf6b6bb7a2a8af84930
Mike Watkins <github@mikewatkins.ca>
2022-11-25 07:47:08 -0800

    gofmt

--- cmd/rid/main.go
+++ cmd/rid/main.go
@@ -5,13 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
-    "strings"
+	"strings"
 
 	"github.com/solutionroute/rid"
 )
 
 var (
-	count   int  = 1
+	count int = 1
 )
 
 func init() {
@@ -19,24 +19,24 @@ func init() {
 }
 
 func main() {
-    flag.Usage = func() {
-        pgm := os.Args[0]
-        fmt.Fprintf(flag.CommandLine.Output(), "usage: %s -c N          # print N rid(s)\n", pgm)
-        fmt.Fprintf(flag.CommandLine.Output(), "       %s 0629p0rqdrw8p # decode one or more rid(s)\n", pgm)
-        // flag.PrintDefaults()
-    }
+	flag.Usage = func() {
+		pgm := os.Args[0]
+		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s -c N          # print N rid(s)\n", pgm)
+		fmt.Fprintf(flag.CommandLine.Output(), "       %s 0629p0rqdrw8p # decode one or more rid(s)\n", pgm)
+		// flag.PrintDefaults()
+	}
 	flag.Parse()
 	args := flag.Args()
-    
-    if count > 1 && len(args) > 0 {
-        fmt.Fprintf(flag.CommandLine.Output(), "error: -c (output) and args (input) both specified; perform only one at a time.\n")
-        flag.Usage()
-        os.Exit(1)
-    }
+
+	if count > 1 && len(args) > 0 {
+		fmt.Fprintf(flag.CommandLine.Output(), "error: -c (output) and args (input) both specified; perform only one at a time.\n")
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	errors := 0
 
-    // If no valid flag, Either attempt to decode string as a rid
+	// If no valid flag, Either attempt to decode string as a rid
 	for _, arg := range args {
 		id, err := rid.FromString(arg)
 		if err != nil {
@@ -44,8 +44,8 @@ func main() {
 			fmt.Printf("[%s] %s\n", arg, err)
 			continue
 		}
-        fmt.Printf("[%s] seconds:%d random:%d machine:[%s] pid:%v time:%v ID{%s}\n", 
-            arg, id.Seconds(), id.Random(), asHex(id.Machine()), id.Pid(), id.Time(), asHex(id[:]))
+		fmt.Printf("[%s] seconds:%d random:%d machine:[%s] pid:%v time:%v ID{%s}\n",
+			arg, id.Seconds(), id.Random(), asHex(id.Machine()), id.Pid(), id.Time(), asHex(id[:]))
 	}
 	if errors > 0 {
 		fmt.Printf("%d error(s)\n", errors)
@@ -53,19 +53,19 @@ func main() {
 	}
 
 	// OR... generate one (or -c value) rid
-    if len(args) == 0 {
-        for c := 0; c < count; c++ {
-            fmt.Fprintf(os.Stdout, "%s\n", rid.New())
-        }
+	if len(args) == 0 {
+		for c := 0; c < count; c++ {
+			fmt.Fprintf(os.Stdout, "%s\n", rid.New())
+		}
 
-    }
+	}
 }
 
 func asHex(b []byte) string {
-    s := []string{}
-    for _, v := range b {
-        s = append(s, fmt.Sprintf("%#x", v))
-    }
-    return strings.Join(s, ", ")
+	s := []string{}
+	for _, v := range b {
+		s = append(s, fmt.Sprintf("%#x", v))
+	}
+	return strings.Join(s, ", ")
 
 }
