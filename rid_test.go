4cce0070948fef728e08405caf05934f6b50b4ba
Mike Watkins <github@mikewatkins.ca>
2022-12-09 07:50:18 -0800

    add json tests, catching a panic in process

--- rid_test.go
+++ rid_test.go
@@ -1,10 +1,12 @@
 package rid
 
-// can verify our custom Base32 character set and encoding here:
+// Verify our custom Base32 character set encoding here:
 // https://cryptii.com/pipes/base32
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -194,12 +196,17 @@ func TestID_UnmarshalText(t *testing.T) {
 			}
 		})
 	}
-	id := New()
-	if err := id.UnmarshalText([]byte("foo")); err != ErrInvalidID {
+
+}
+
+func TestID_UnmarshalTextError(t *testing.T) {
+	id := nilID
+	if err := id.UnmarshalText([]byte("invalid")); err != ErrInvalidID {
 		t.Errorf("ID.UnmarshalText() error = %v, wantErr %v", err, ErrInvalidID)
 	}
-	if err := id.UnmarshalText([]byte("foo")); err != nil && !id.IsNil() {
-		t.Errorf("ID.UnmarshalText() error = %v, want nil ID, got %v", err, id)
+	id = New() // make a non nil ID
+	if err := id.UnmarshalText([]byte("foo")); id != nilID {
+		t.Errorf("ID.UnmarshalText() want nil ID, ErrInvalidID, got %v, %v", id, err)
 	}
 }
 
@@ -270,11 +277,109 @@ func TestFromBytes_Invariant(t *testing.T) {
 	}
 }
 
+type jsonType struct {
+	ID  *ID
+	Str string
+}
+
+func TestIDJSONMarshaling(t *testing.T) {
+	// 062ektdeb6039z5masctt333 ts:1670371716697 rtsig:[0x80] random: 58259961960877 | time:2022-12-06 16:08:36.697 -0800 PST ID{0x1,0x84,0xe9,0xe9,0xae,0x59,0x80,0x34,0xfc,0xb4,0x56,0x59,0xad,0xc,0x63}
+	id := ID{0x1, 0x84, 0xe9, 0xe9, 0xae, 0x59, 0x80, 0x34, 0xfc, 0xb4, 0x56, 0x59, 0xad, 0xc, 0x63}
+	v := jsonType{ID: &id, Str: "test"}
+	data, err := json.Marshal(&v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := string(data), `{"ID":"062ektdeb6039z5masctt333","Str":"test"}`; got != want {
+		t.Errorf("json.Marshal() = %v, want %v", got, want)
+	}
+}
+
+func TestIDJSONUnmarshaling(t *testing.T) {
+	data := []byte(`{"ID":"062ektdeb6039z5masctt333","Str":"test"}`)
+	v := jsonType{}
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := ID{0x1, 0x84, 0xe9, 0xe9, 0xae, 0x59, 0x80, 0x34, 0xfc, 0xb4, 0x56, 0x59, 0xad, 0xc, 0x63}
+	if got := *v.ID; got.Compare(want) != 0 {
+		t.Errorf("json.Unmarshal() = %v, want %v", got, want)
+	}
+	// should not fail
+	err = json.Unmarshal([]byte(`null`), &v)
+	if err != nil {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, nil)
+	}
+}
+
+func TestIDJSONUnmarshalingError(t *testing.T) {
+	v := jsonType{}
+	// callers are responsible for forcing lower case input for Base32
+	// otherwise valid id:
+	err := json.Unmarshal([]byte(`{"ID":"062EKTDEB6039Z5MASCTT333"}`), &v)
+	if err != ErrInvalidID {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, ErrInvalidID)
+	}
+	// too short
+	err = json.Unmarshal([]byte(`{"ID":"062ektdeb6039z5masctt"}`), &v)
+	if err != ErrInvalidID {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, ErrInvalidID)
+	}
+	// ID's methods do not know about Base64 encoded data; up to you
+	// to use FromString64 as needed
+	err = json.Unmarshal([]byte(`{"ID":"AYT3PAVLn1207oRutpGK"}`), &v)
+	if err != ErrInvalidID {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, ErrInvalidID)
+	}
+	err = json.Unmarshal([]byte(`{"ID":1}`), &v)
+	if err != ErrInvalidID {
+		t.Errorf("json.Unmarshal() err=%v, want %v", err, ErrInvalidID)
+	}
+}
+
+func TestIDDriverValue(t *testing.T) {
+	id := ID{0x1, 0x84, 0xe9, 0xe9, 0x94, 0xa0, 0x26, 0x35, 0xc3, 0x9d, 0x69, 0x16, 0xc7, 0x5f, 0x4b}
+	got, err := id.Value()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want := "062ektcmm0k3bgwxd4bceqtb"; got != want {
+		t.Errorf("Value() = %v, want %v", got, want)
+	}
+}
+
 func TestIDDriverScan(t *testing.T) {
 	// 062ektcmm0k3bgwxd4bceqtb ts:1670371710112 sig:0x26 rnd: 3874113179148050251 2022-12-06 16:08:30.112 -0800 PST
 	// ID{0x1,0x84,0xe9,0xe9,0x94,0xa0,0x26,0x35,0xc3,0x9d,0x69,0x16,0xc7,0x5f,0x4b}
 	got := ID{}
 	err := got.Scan("062ektcmm0k3bgwxd4bceqtb")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := ID{0x1, 0x84, 0xe9, 0xe9, 0x94, 0xa0, 0x26, 0x35, 0xc3, 0x9d, 0x69, 0x16, 0xc7, 0x5f, 0x4b}
+	if got.Compare(want) != 0 {
+		t.Errorf("Scan() = %v, want %v", got, want)
+	}
+}
+
+func TestIDDriverScanError(t *testing.T) {
+	id := ID{}
+	if got, want := id.Scan(0), errors.New("rid: scanning unsupported type: int"); !reflect.DeepEqual(got, want) {
+		t.Errorf("Scan() err=%v, want %v", got, want)
+	}
+	if got, want := id.Scan("0"), ErrInvalidID; got != want {
+		t.Errorf("Scan() err=%v, want %v", got, want)
+		if id != nilID {
+			t.Errorf("Scan() id=%v, want %v", got, nilID)
+		}
+	}
+}
+
+func TestIDDriverScanByteFromDatabase(t *testing.T) {
+	got := ID{}
+	bs := []byte("062ektcmm0k3bgwxd4bceqtb")
+	err := got.Scan(bs)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -369,27 +474,36 @@ func TestFromString64(t *testing.T) {
 	}
 }
 
-func Test_randUint32(t *testing.T) {
-	one := randUint32()
-	another := randUint32()
-	if one == another {
-		t.Errorf("randUint32() = %v should not equal another: %v", one, another)
-	}
-}
-
-func Test_randUint64(t *testing.T) {
-	one := randUint64()
-	another := randUint64()
-	if one == another {
-		t.Errorf("randUint64() = %v should not equal another: %v", one, another)
+func Test_runtime_randUint64(t *testing.T) {
+	// On a test machine generating 100,000,000 numbers took 24s (4.167 million per second),
+	// with zero collisions. 24 seconds = 24,000 milliseconds; 100,000,000/24,000
+	// equals ~4,167 unique random generations per millisecond are required.
+	// Set at 1,000,000:
+	count := 1000000
+	exists := make(map[uint64]bool)
+	for i := 0; i < count; i++ {
+		r := runtime_randUint64()
+		if exists[r] {
+			t.Errorf("Duplicate random number %d generated within %d attempts", r, count)
+		}
+		exists[r] = true
 	}
 }
 
 func Test_runtimeSignature(t *testing.T) {
-	// should not be a nil value
-	var nilMachineID = make([]byte, 1)
+	// rid.rtsig is set once only and intentionally so in case the platform's
+	// machine ID is missing (common on some Linux) and no hostname is returned,
+	// not even 'localhost', a random number will be issued. That should be rare.
+	//
+	// All we are testing is that rtsig should not be a nil value
+	var nilMachineID [1]byte
 	if got := runtimeSignature(); reflect.DeepEqual(got, nilMachineID) {
 		t.Errorf("randomMachineId() = %v, want %v, shouldn't be nil", got, nilMachineID)
+	}
+	// this should not fail but might fail on CI platforms like Github
+	r := runtimeSignature()
+	if !reflect.DeepEqual(r, rtsig) {
+		t.Errorf("runtimeSignature() = %v does not match pkg init %v.", r, rtsig)
 	}
 }
 
@@ -450,13 +564,8 @@ func ExampleNew() {
 
 func ExampleNewWithTimestamp() {
 	id := NewWithTimestamp(uint64(time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC).UnixMilli()))
-	fmt.Printf(`ID:
-    String()  %s
-    Timestamp() %d
-    RuntimeSignature() %v 
-    Random()  %d 
-    Time()    %v
-    Bytes()   %3v\n`, id.String(), id.Timestamp(), id.RuntimeSignature(), id.Random(), id.Time().UTC(), id.Bytes())
+	fmt.Printf(`ID: Timestamp() %d Time() %v`, id.Timestamp(), id.Time().UTC())
+	// Output: ID: Timestamp() 1577836800000 Time() 2020-01-01 00:00:00 +0000 UTC
 }
 
 func ExampleFromString() {
@@ -465,6 +574,7 @@ func ExampleFromString() {
 		panic(err)
 	}
 	fmt.Println(id.Timestamp(), id.Random())
+	// Output: 1670467144866 16427575998925264646
 }
 
 func ExampleFromString64() {
@@ -473,5 +583,5 @@ func ExampleFromString64() {
 		panic(err)
 	}
 	fmt.Println(id.Timestamp(), id.Random())
-	// 1670467328750 8633952041140987771
+	// Output: 1670467328750 8633952041140987771
 }
