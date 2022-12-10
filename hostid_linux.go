ebbbdc9bf432719c6e3d896377fea23373d89213
Mike Watkins <github@mikewatkins.ca>
2022-12-09 16:52:58 -0800

    Switch to more ubiquitous machine ID sources

--- hostid_linux.go
+++ hostid_linux.go
@@ -4,10 +4,11 @@ package rid
 
 import "os"
 
+// see https://0pointer.de/blog/projects/ids.html
 func readPlatformMachineID() (string, error) {
-	b, err := os.ReadFile("/etc/machine-id")
+	b, err := os.ReadFile("/var/lib/dbus/machine-id")
 	if err != nil || len(b) == 0 {
-		b, err = os.ReadFile("/sys/class/dmi/id/product_uuid")
+		b, err = os.ReadFile("/proc/sys/kernel/random/boot_id")
 	}
 	return string(b), err
 }
