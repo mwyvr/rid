a7fb2bb870cb5a4371c6f009c71c5315aefbaf34
Mike Watkins <github@mikewatkins.ca>
2023-02-06 17:44:36 -0800

    fix: avoid compiler optimizations in benchmark

--- eval/bench/bench_test.go
+++ eval/bench/bench_test.go
@@ -14,64 +14,89 @@ import (
 )
 
 // rid ids incorporate crypto/rand generated numbers
+var resultRID rid.ID
+
 func BenchmarkRid(b *testing.B) {
+	var r rid.ID
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			_ = rid.New()
+			r = rid.New()
 		}
+		resultRID = r
 	})
 }
 
 // https://github.com/rs/xid
 // xid uses a random-initialized (once only) monotonically increasing counter
+var resultXID xid.ID
+
 func BenchmarkXid(b *testing.B) {
+	var r xid.ID
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			_ = xid.New()
+			r = xid.New()
 		}
+		resultXID = r
 	})
 }
 
 // https://github.com/segmentio/ksuid
 // ksuid ids incorporate crypto/rand generated numbers
+
+var resultKSUID ksuid.KSUID
+
 func BenchmarkKsuid(b *testing.B) {
+	var r ksuid.KSUID
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			_ = ksuid.New()
+			r = ksuid.New()
 		}
+		resultKSUID = r
 	})
 }
 
 // uuid ids incorporate crypto/rand generated numbers
+var resultUUID guuid.UUID
+
 func BenchmarkGoogleUuid(b *testing.B) {
+	var r guuid.UUID
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			// https://pkg.go.dev/github.com/google/UUID#NewRandom
 			// uuid v4, equiv to NewRandom()
-			_ = guuid.New()
+			r = guuid.New()
 		}
+		resultUUID = r
 	})
 }
 
 // as configured here, for a good comparison, ulid ids incorporate crypto/rand
 // generated numbers
+var resultULID ulid.ULID
+
 func BenchmarkUlid(b *testing.B) {
+	var r ulid.ULID
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			t := time.Now().UTC()
 			entropy := rand.New(rand.NewSource(t.UnixNano()))
-			_ = ulid.MustNew(ulid.Timestamp(t), entropy)
+			r = ulid.MustNew(ulid.Timestamp(t), entropy)
 		}
+		resultULID = r
 	})
 }
 
 // https://github.com/kjk/betterguid
 // like rs/xid, uses a monotonically incrementing counter rather than
 // true randomness
+var resultBGUID string
+
 func BenchmarkBetterguid(b *testing.B) {
+	var r string
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			_ = betterguid.New()
+			r = betterguid.New()
 		}
+		resultBGUID = r
 	})
 }
