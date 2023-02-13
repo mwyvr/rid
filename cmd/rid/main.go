bafdb13591f674fdfd8aa819395f9792fffff72a
Mike Watkins <github@mikewatkins.ca>
2023-02-13 08:51:16 -0800

    improve flags and usage

--- cmd/rid/main.go
+++ cmd/rid/main.go
@@ -11,22 +11,23 @@ import (
 )
 
 func main() {
-	count := flag.Int("c", 1, "Generate n IDs")
+	count := 1
+	flag.IntVar(&count, "c", count, "Generate N-count IDs")
 	flag.Usage = func() {
 		fs := flag.CommandLine
 		fcount := fs.Lookup("c")
 		fmt.Printf("Usage: rid\n\n")
 		fmt.Printf("Options:\n")
-		fmt.Printf("  rid dgm3w9sh9f5flv5s\t\tDecode the ID supplied\n")
-		fmt.Printf("  rid -%s N\t\t\t%s default:%s\n\n", fcount.Name, fcount.Usage, fcount.DefValue)
-		fmt.Printf("With no parameters, rid generates 1 random ID encoded as Base32.\n")
+		fmt.Printf("  rid dgm3w9sh9f5flv5s\t\tDecode the supplied Base32 ID\n")
+		fmt.Printf("  rid -%s N\t\t\t%s default: %s\n\n", fcount.Name, fcount.Usage, fcount.DefValue)
+		fmt.Printf("With no parameters, rid generates %s random ID encoded as Base32.\n", fcount.DefValue)
 		fmt.Printf("Generate and inspect 4 random IDs using Linux/Unix command substituion:\n")
 		fmt.Printf("  rid `rid -c 4`\n")
 	}
 	flag.Parse()
 	args := flag.Args()
 
-	if *count > 1 && len(args) > 0 {
+	if count > 1 && len(args) > 0 {
 		fmt.Fprintf(flag.CommandLine.Output(),
 			"rid: Error, cannot generate ID(s) and inspect at the same time. Use command substituion. \n")
 		flag.Usage()
@@ -46,7 +47,7 @@ func main() {
 		}
 	} else {
 		// generate one or -c N ids
-		for c := 1; c <= *count; c++ {
+		for c := 1; c <= count; c++ {
 			fmt.Fprintf(os.Stdout, "%s\n", rid.New())
 		}
 	}
